084444882ad7cf44166d19c40621d1fb587c1c3b
Anthony-Tresallet <tresallet.anthony@gmail.com>
2022-06-25 11:56:11 +0200

    first commit

---
+++ libraries/sketch.js
@@ -0,0 +1,87 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+let dcgan;
+let button;
+let cnv;
+var clicks = 0;
+
+function preload() {
+  dcgan = ml5.DCGAN("model/fullData/manifest.json");
+}
+
+function setup() {
+  
+  cnv = createCanvas(2400, 3508);
+  background(255);
+  smooth();
+
+
+
+  // Reduce size of preview
+  let scalePreview = 0.25;
+  cnv = document.getElementById("defaultCanvas0");
+  cnv.style.width = round(width * scalePreview) + "px";
+  cnv.style.height = round(height * scalePreview) + "px";
+
+  
+  button = createButton("Génère ton image ma race");
+  button.mousePressed(GANGeneration);
+  
+  GANGeneration();
+// Button to generate an image
+}
+
+function draw() {
+  
+  textSize(40);
+  textStyle(ITALIC);
+  textFont("Times");
+  textAlign(LEFT);
+
+  text("À la Recherche", 60, width + 40);
+
+  textAlign(CENTER);
+  textStyle(NORMAL);
+  text("Génération", width / 2, width + 40);
+
+  textAlign(RIGHT);
+  textStyle(NORMAL);
+  stroke(255);
+  strokeWeight(20)
+  text(clicks, width - 60, width + 40);
+  noStroke();
+}
+
+function keyPressed() {
+  if (keyCode === 83) {
+    // Saves the canvas as an image
+    save(clicks+"_Generations_ÀlaRecherche.jpg");
+  }
+}
+
+const imageNumber = 7;
+const firstAndLastOffset = 100;
+const startOffset = 0;
+const offset = 400;
+
+function GANGeneration() {
+  Array.from(Array(imageNumber).keys()).forEach(index => dcgan.generate(displayImageGeneric(index)));
+}
+
+function displayImageGeneric(index) {
+  return function (err, result) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    clicks++;
+    imageMode(CENTER);
+    let size = width - offset * index + startOffset;
+    if (index === 0 || index === imageNumber - 1)
+      size -= firstAndLastOffset;
+    image(result.image, width / 2, width / 2, size, size);
+  }
+}
